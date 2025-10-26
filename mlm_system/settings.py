2c13ccef10755ec4357623ed404de6b62a59f185
MAG <208105026+mag8888@users.noreply.github.com>
2025-10-26 13:20:34 +0300

    🔧 Убраны предупреждения staticfiles - система работает без внешних файлов

--- mlm_system/settings.py
+++ mlm_system/settings.py
@@ -146,7 +146,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # WhiteNoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
