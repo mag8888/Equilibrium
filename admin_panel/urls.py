8877e38e2404002cc333d6ef5dacb6fd1ce7db78
MAG <208105026+mag8888@users.noreply.github.com>
2025-10-26 13:52:56 +0300

    🔄 НОВЫЙ URL: /mindmap/ для горизонтальной майнд-карты

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', demo_views.admin_demo_dashboard, name='dashboard'),
     path('users/', demo_views.admin_demo_users, name='users_list'),
     path('structure/', demo_views.admin_demo_structure_new, name='structure_view'),  # Новая горизонтальная майнд-карта
+    path('mindmap/', demo_views.admin_demo_structure_new, name='mindmap_view'),  # Альтернативный URL для майнд-карты
 ]
