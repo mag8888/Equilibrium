f129ec10f6e18a5fd3dace9a0aba3df812ed67ff
MAG <208105026+mag8888@users.noreply.github.com>
2025-10-28 15:26:33 +0300

    🛠️ Авто-migrate при старте WSGI (страховка для Railway) v88

--- mlm_system/wsgi.py
+++ mlm_system/wsgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mlm_system.settings')
 
 application = get_wsgi_application()
+
+# Авто-применение миграций на старте (защита, если платформа игнорирует start.sh)
+try:
+    call_command('migrate', interactive=False, run_syncdb=True, verbosity=0)
+except Exception:
+    # Не мешаем запуску, если миграции не могут выполниться здесь
+    pass
