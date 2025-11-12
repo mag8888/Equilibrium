17d009c4c43141e77bb4701cbde6fc1a5ab8b436
MAG <208105026+mag8888@users.noreply.github.com>
2025-11-12 07:24:23 +0300

    Improve admin dashboard design and fix API permissions

--- backend/equilibrium_backend/settings.py
+++ backend/equilibrium_backend/settings.py
@@ -166,7 +166,7 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.AllowAny",  # По умолчанию разрешаем доступ
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
